fcc35c90bb0c69fca6a5b56cdfd7230b4855dcb5
TyteQ <totek.254@gmail.com>
2018-10-30 00:37:20 +0100

    Add files via upload

--- AI1/AlgorytmGenetyczny.xaml.cs
+++ AI1/AlgorytmGenetyczny.xaml.cs
@@ -22,113 +22,99 @@ namespace AI1
     /// </summary>
     public sealed partial class AlgorytmGenetyczny : Page
     {
-        private double Eval;
-        private double Fitness;
-        private double RFitness;
-        private double RMin;
-        private double RMax;
-        private double[] Gene = null;
-
-        // --- konstruktor
-        public AlgorytmGenetyczny(int g, double ev, double f, double rf, double rmin, double rmax)
-        {
-            Eval = ev;
-            Fitness = f;
-            RFitness = rf;
-            RMin = rmin;
-            RMax = rmax;
-            Gene = new double[g];
-
-            for (int i = 1; i <= g; i++)
-            {
-                Gene[i - 1] = 0;
-            }
-            this.InitializeComponent();
-        }
-        public void setEval(double n)
-        {
-            Eval = n;
-        }
-
-        public void setFitness(double n)
-        {
-            Fitness = n;
-        }
-
-        public void setRFitness(double n)
-        {
-            RFitness = n;
-        }
-
-        public void setRMin(double n)
-        {
-            RMin = n;
-        }
-
-        public void setRMax(double n)
-        {
-            RMax = n;
-        }
-
-        public void set_i_Gene(int i, double n)
-        {
-            Gene[i] = n;
-        }
-        public void setAllGeneRandom(double Low, double High, bool n, int ile)
-        {
-            for (int i = 1; i <= ile; i++)
-            {
-                    Random generator = new Random();
-                if (n == true)
-                {
-                    this.Gene[i - 1] = (generator.Next((int)(High - Low + 1)) + Low);
-                }
-                else
-                {
-                    this.Gene[i - 1] = (double)((generator.NextDouble() * (High - Low)) + Low);
-                }
-            }
-        }
-
-        public double getEval()
-        {
-            return Eval;
-        }
-
-        public double getFitness()
-        {
-            return Fitness;
-        }
-
-        public double getRFitness()
-        {
-            return RFitness;
-        }
-
-        public double getRMin()
-        {
-            return RMin;
-        }
-
-        public int getGeneLength()
-        {
-            return this.Gene.Length;
-        }
-
-        public double getRMax()
-        {
-            return RMax;
-        }
-
-        public double get_i_Gene(int i)
-        {
-            return Gene[i];
-        }
-
-
         public AlgorytmGenetyczny()
         {
             this.InitializeComponent();
+        }
+        bool iInt;
+        double Delta;
+        double PCrossOver;
+        double PMutation;
+        int PopSize;
+        int NumberOfVariables;
+        double MaxValueOfEachGene;
+        double MinValueOfEachGene;
+        int NumberOfIteration;
+        int Iteration;
+        Population Pop1;
+        Genotype TheBest;
+        Genotype NowTheBest;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            iInt = (bool)false;
+            Delta = (double)0.00000001;
+            PCrossOver = (double)0.7;
+            PMutation = (double)0.005;
+            PopSize = 100;
+            NumberOfVariables = 10;
+            MaxValueOfEachGene = 10;
+            MinValueOfEachGene = 0;
+            NumberOfIteration = 10000;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TheBest = new Genotype(NumberOfVariables, 0, 0, 0, 0, 0);
+            NowTheBest = new Genotype(NumberOfVariables, 0, 0, 0, 0, 0);
+            Pop1 = new Population(PopSize, PCrossOver, PMutation, Delta,
+                    NumberOfVariables, MinValueOfEachGene, MaxValueOfEachGene, iInt);
+            Pop1.initPop(PopSize, NumberOfVariables,
+                    MinValueOfEachGene, MaxValueOfEachGene, iInt);
+            Pop1.ComputeEval();
+            Pop1.ComputeFitness();
+            Pop1.ComputeRelativeFitness();
+            TheBest = Pop1.GetTheBest();
+            NowTheBest = Pop1.GetTheBest();
+            Iteration = 0;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i <= NumberOfIteration; i++)
+            {
+                Iteration++;
+                Pop1.CrossOver();
+                Pop1.Mutation(iInt);
+                Pop1.ComputeEval();
+                Pop1.ComputeFitness();
+                Pop1.ComputeRelativeFitness();
+                Pop1.ElitistSelection();
+                Pop1.ComputeEval();
+                Pop1.ComputeFitness();
+                Pop1.ComputeRelativeFitness();
+                NowTheBest = Pop1.GetTheBest();
+                if (NowTheBest.getEval() > TheBest.getEval())
+                {
+                    Pop1.set_i_Genotype(Pop1.GetTheNumberOfTheWorst(), TheBest);
+                }
+                if (NowTheBest.getEval() < TheBest.getEval())
+                {
+                    for (int n = 0; n <= (NumberOfVariables - 1); n++)
+                    {
+                        TheBest.set_i_Gene(n, NowTheBest.get_i_Gene(n));
+                        TheBest.setEval(NowTheBest.getEval());
+                    }
+                }
+                textBlock1.Text="Iteration = " + Iteration + "   Evaluate = " + TheBest.getEval();
+            }
+
+            FileStream writeStream;
+            try
+            {
+                writeStream = new FileStream("TheBest.txt", FileMode.OpenOrCreate);
+                BinaryWriter writeBinay = new BinaryWriter(writeStream);
+                textBlock2.Text = "Writing data to the stream.";
+                for (int i = 0; i < NumberOfVariables; i++)
+                {
+                    writeBinay.Write(TheBest.get_i_Gene(i - 1));
+                }
+            }
+            catch (Exception ex)
+            {
+                textBlock2.Text = ex.ToString();
+            }
         }
     }
 }
---
+++ AI1/Population.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI1
+{
+    class Population
+    {
+        // --- konstruktor
+        public Population(int r, double pc, double pm, double del, int nv, double L,
+                          double H, bool i)
+        {
+            Genotype[] Pop = new Genotype[r];
+            Genotype[] TPop = new Genotype[r];
+            PopS = r;
+            PCross = pc;
+            PMut = pm;
+            Delta = del;
+            NV = nv;
+            Low = L;
+            High = H;
+            iInt = i;
+        }
+
+        // --- metody
+
+        public void initPop(int r, int g, double Low, double High, bool n)
+        {
+            for (int i = 1; i <= r; i++)
+            {
+                Pop[i - 1] = new Genotype(r, 0, 0, 0, 0, 0);
+                Pop[i - 1].setAllGeneRandom(Low, High, n, g);
+                TPop[i - 1] = new Genotype(r, 0, 0, 0, 0, 0);
+                TPop[i - 1].setAllGeneRandom(Low, High, n, g);
+            }
+        }
+
+        public double Evaluate(Genotype g)
+        {
+            double Sum = 0;
+            for (int i = 1; i <= this.NV; i++)
+            {
+                Sum = Sum + g.get_i_Gene(i - 1);
+            }
+            return Sum;
+        }
+
+        public void ComputeEval()
+        {
+            double tmp;
+            for (int i = 1; i <= this.PopS; i++)
+            {
+                tmp = this.Evaluate(Pop[i - 1]);
+                this.Pop[i - 1].setEval(tmp);
+            }
+        }
+
+        public void ComputeFitness()
+        {
+            double MaxEval = this.Pop[0].getEval();
+            for (int i = 1; i <= (this.PopS - 1); i++)
+            {
+                if (this.Pop[i].getEval() > MaxEval)
+                {
+                    MaxEval = this.Pop[i].getEval();
+                }
+            }
+            for (int i = 0; i <= (this.PopS - 1); i++)
+            {
+                this.Pop[i].setFitness(MaxEval - this.Pop[i].getEval() + this.Delta);
+            }
+        }
+
+        public void set_i_Genotype(int i, Genotype g)
+        {
+            for (int j = 0; j <= (this.NV - 1); j++)
+            {
+                this.Pop[i].set_i_Gene(j, g.get_i_Gene(j));
+            }
+            this.Pop[i].setEval(g.getEval());
+            this.Pop[i].setFitness(g.getFitness());
+            this.Pop[i].setRFitness(g.getRFitness());
+            this.Pop[i].setRMax(g.getRMax());
+            this.Pop[i].setRMin(g.getRMin());
+        }
+
+        public void ComputeRelativeFitness()
+        {
+            double Sum = 0;
+            for (int i = 1; i <= this.PopS; i++)
+            {
+                Sum = Sum + this.Pop[i - 1].getFitness();
+            }
+            for (int i = 1; i <= this.PopS; i++)
+            {
+                this.Pop[i - 1].setRFitness(this.Pop[i - 1].getFitness() / Sum);
+            }
+        }
+
+        public Genotype GetTheBest()
+        {
+            double Max = this.Pop[0].getEval();
+            Genotype g = new Genotype(this.NV, 0, 0, 0, 0, 0);
+            int p = 0;
+            for (int i = 1; i <= (this.PopS - 1); i++)
+            {
+                if (this.Pop[i].getEval() < Max)
+                {
+                    Max = this.Pop[i].getEval();
+                    p = i;
+                }
+            }
+            for (int i = 0; i <= (this.NV - 1); i++)
+            {
+                g.set_i_Gene(i, this.Pop[p].get_i_Gene(i));
+            }
+            g.setEval(this.Pop[p].getEval());
+            g.setFitness(this.Pop[p].getFitness());
+            g.setRFitness(this.Pop[p].getRFitness());
+            g.setRMax(this.Pop[p].getRMax());
+            g.setRMin(this.Pop[p].getRMin());
+
+            return g;
+        }
+
+        public void CrossOver()
+        {
+            double Los;
+            int n = 0;
+            int[] pp = new int[this.PopS];
+            for (int i = 0; i <= (this.PopS - 1); i++)
+            {
+                Random generator = new Random();
+                Los = generator.NextDouble();
+                if (Los < this.PCross)
+                {
+                    n++;
+                    pp[n] = i;
+                }
+            }
+            int t1;
+            int t2;
+            int p1;
+            int p2;
+            int cut;
+            double tmp;
+
+            Random Generator = new Random();
+
+            while (n > 1)
+            {
+                t1 = Generator.Next(n) + 1;
+                p1 = pp[t1 - 1];
+                pp[t1 - 1] = pp[n - 1];
+                n--;
+                t2 = Generator.Next(n) + 1;
+                p2 = pp[t2 - 1];
+                pp[t2 - 1] = pp[n - 1];
+                n--;
+                //----- tu krzyzujemy p1 z p2
+                cut = Generator.Next(this.NV - 1);
+                for (int i = cut + 1; i <= (this.NV - 1); i++)
+                {
+                    tmp = this.Pop[p1].get_i_Gene(i);
+                    this.Pop[p1].set_i_Gene(i, this.Pop[p2].get_i_Gene(i));
+                    this.Pop[p2].set_i_Gene(i, tmp);
+                }
+                //---------------------------
+            }
+        }
+
+        public void Mutation(bool iInt)
+        {
+            double Los;
+            if (iInt == true)
+            {
+                Random Generator = new Random();
+                for (int i = 0; i <= (this.PopS - 1); i++)
+                {
+                    for (int j = 0; j <= (this.NV - 1); j++)
+                    {
+                        Los = (double)Generator.NextDouble();
+                        if (Los < this.PMut)
+                        {
+                            
+                            double t = Generator.Next((int)(this.High - this.Low + 1)) + this.Low;
+                            Pop[i].set_i_Gene(j, t);
+                        }
+                    }
+                }
+            }
+            if (iInt == false)
+            {
+                Random Generator = new Random();
+                for (int i = 0; i <= (this.PopS - 1); i++)
+                {
+                    for (int j = 0; j <= (this.NV - 1); j++)
+                    {
+                        Los = (double)Generator.NextDouble();
+                        if (Los < this.PMut)
+                        {
+                            double t = this.High - this.Low;
+                            t = (double)((Generator.NextDouble() * t) + this.Low);
+                            Pop[i].set_i_Gene(j, t);
+                        }
+                    }
+                }
+            }
+        }
+
+        public int GetTheNumberOfTheWorst()
+        {
+            int MinI = 0;
+            double Min = this.Pop[MinI].getEval();
+            for (int i = 1; i <= (this.PopS - 1); i++)
+            {
+                if (this.Pop[i].getEval() > Min)
+                {
+                    Min = this.Pop[i].getEval();
+                    MinI = i;
+                }
+            }
+            return MinI;
+        }
+
+        public void ElitistSelection()
+        {
+            this.Pop[0].setRMin(0);
+            this.Pop[0].setRMax(this.Pop[0].getRMin() + this.Pop[0].getRFitness());
+            for (int i = 1; i <= (this.PopS - 1); i++)
+            {
+                this.Pop[i].setRMin(this.Pop[i - 1].getRMax());
+                this.Pop[i].setRMax(this.Pop[i].getRMin() + this.Pop[i].getRFitness());
+            }
+            for (int i = 0; i <= (this.PopS - 1); i++)
+            {
+                Random Generator = new Random();
+                double Los = (double)Generator.NextDouble();
+                for (int j = 0; j <= (this.PopS - 1); j++)
+                {
+                    if ((Los >= this.Pop[j].getRMin()) && (Los < this.Pop[j].getRMax()))
+                    {
+                        this.TPop[i] = this.Pop[j];
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i <= (this.PopS - 1); i++)
+            {
+                this.Pop[i] = this.TPop[i];
+            }
+        }
+
+        public void FanSelection()
+        {
+        }
+        // --- pola skladowe
+        private int PopS;
+        private double PCross;
+        private double PMut;
+        private double Delta;
+        private int NV;
+        private double Low;
+        private double High;
+        private Genotype[] Pop = new Genotype[100];
+        private Genotype[] TPop = new Genotype[100];
+        private bool iInt;
+    }
+}
